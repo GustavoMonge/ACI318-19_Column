7286d82cfc523c776a02fd7c0d3c09adb0a2970f
Gustavo Monge <GustavoMonge@users.noreply.github.com>
2022-11-02 22:35:18 -0600

    Paul Hudson, Tutorial 4, Drawing SwiftUI, Flower Drawing with slider to modify the drawing
    Core Graphics Flower Drawing with slider to modify the drawing

--- ACI318-19_Column/ContentView.swift
+++ ACI318-19_Column/ContentView.swift
@@ -7,67 +7,48 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth = 100.0
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        let petalHeight = rect.width/2
         
+        // stride is a function
+        for number in stride(from: 0.0, to: (Double.pi * 2), by: (Double.pi / 8)) {
+            
+            let rotation = CGAffineTransform(rotationAngle: CGFloat(number))
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0.0, width: petalWidth, height: Double(petalHeight)))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
         return path
     }
+    
 }
 
-struct Arc: InsettableShape {
-    
-    let startAngle: Angle
-    let endAngle : Angle
-    let clockwise: Bool
-    // variable to implement strokrBorder and conform to InsettableShape Protocol
-    var insetAmount: CGFloat = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: CGFloat(rect.midY)), radius: CGFloat(min((rect.width/2 - insetAmount), (rect.height/2 - insetAmount))), startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-        
-        return path
-    }
-    
-    // func to be able to use .strokeBorder
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-}
 
 struct ContentView: View {
-var body: some View {
-    
-    Arc(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(90), clockwise: true)
-                .strokeBorder(Color.red, style: StrokeStyle(lineWidth: 40))
-                .frame(width: 300, height: 300)
-    
-    
-    
-//        Triangle()
-// //           .fill(Color.red)
-//            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-//            .frame(width: 300, height: 300)
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    var body: some View {
+        VStack{
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .stroke(Color.red, lineWidth: 1.0)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+        }
     }
 }
     
-    
-    
-    
+
     
     
     
