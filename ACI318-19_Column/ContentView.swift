3a02bbe20f4ffdedcbe9a2c1e9e8932b865e8c71
Gustavo Monge <GustavoMonge@users.noreply.github.com>
2022-11-02 19:34:47 -0600

    Paul Hudson, Tutorial 2, Drawing SwiftUI, Part: a, Tringle
    Core Graphics Draw Triangle

--- ACI318-19_Column/ContentView.swift
+++ ACI318-19_Column/ContentView.swift
@@ -7,75 +7,35 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            
-        }
-        .stroke(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
     }
 }
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //        GeometryReader { geometry in
-//
-//        let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
-//        let myView = UIView(frame: rect)
-//
-//        let shape = RoundedRectangle(cornerRadius:20)
-//        shape.fill().foregroundColor(.red)
-//        shape.strokeBorder(lineWidth: 3)
-//        shape.padding(20)
-//
-//            Text("Hola Gustavo").font( Font.system(size: 50)).colorInvert()
-//        }
-//    }
+
+
+struct ContentView: View {
+    var body: some View {
+        Triangle()
+ //           .fill(Color.red)
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
+    }
+}
     
     
-
-
-
-//private struct DrawingConstants {
-//    static let cornerRadious: CGFloat = 20
-//    static let lineWidth: CGFloat = 3
-//    static let fontScale: CGFloat = 0.8
-//}
-//
-//
-//
-//struct Column: View {
-//    var body: some View{
-//        ZStack{
-//        let rect = CGRect(x: 10, y: 10, width: 50, height: 100)
-//            let myView = UIView(frame: rect).backgroundColor? .ciColor.blue
-//
-//        }
-//    }
-//}
-
-
-
+    
+    
+    
+    
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
